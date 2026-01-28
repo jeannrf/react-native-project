080821080ecdd24acbb3790755f1e52cfd49ea30
Jeanpier <jeanpier.robles.f@uni.pe>
2026-01-27 22:26:12 -0500

    Mejora de la app

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, esta es la app de metacritic </Text>
+      <Image source={{ uri: 'https://files.idyllic.app/files/static/2494807?width=256&optimizer=image' }} style={{ width: 256, height: 256 }} />
+      <Text style={{ color: 'white' }}>Hola, esta es la app de metacritic :D </Text>
       <StatusBar style="auto" />
     </View>
   );
