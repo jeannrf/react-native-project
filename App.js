919201f5356fa13fadf192e2ed129a405aabbe5d
Jeanpier <jeanpier.robles.f@uni.pe>
2026-01-27 23:46:41 -0500

    Añadí el buttom styleable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
 
 import icon from './assets/icon.png';
 
@@ -7,9 +7,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Image source={{ uri: 'https://files.idyllic.app/files/static/2494807?width=256&optimizer=image' }} style={{ width: 256, height: 256 }} />
       <Text style={{ color: 'white' }}>Hola, esta es la app de metacritic :D </Text>
       <Button title="Pulsa aquí" onPress={() => alert('Has pulsado el botón')} />
+      <TouchableHighlight
+        underlayColor="red"
+        onPress={() => alert('Has pulsado el botdasdadón')}
+      >
+        <Text>Pulsa aquí</Text>
+      </TouchableHighlight>
     </View>
   );
 }
